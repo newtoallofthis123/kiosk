e8d035b1062b1350560b042e83a4245659ddce6e
newtoallofthis123 <noobscience123@gmail.com>
2024-06-27 00:19:05 +0530

    Fix Disease Detection

--- frontend/app/probs/page.tsx
+++ frontend/app/probs/page.tsx
@@ -1,20 +1,24 @@
-"use client"
+'use client';
 
-import ChatResponse from "@/components/base/chatResponse";
-import { useSearchParams } from "next/navigation"
+import ChatResponse from '@/components/base/chatResponse';
+import { useSearchParams } from 'next/navigation';
 
 export default function Probs() {
-    const searchParams = useSearchParams()
+    const searchParams = useSearchParams();
     const problems = searchParams.get('problems') || '';
 
-    const problemList = problems.split(";")
+    const problemList = problems.split(';');
     const disease = searchParams.get('disease') || 'Common Cold';
 
+    //It has been determined that the patient has been diagnosed with the following: ${disease}.
     const prompt = `Imagine you are a doctor and you are treating a 
-patient with the following problems: ${problemList.join(", ")}.
+patient with the following problems: ${problemList.join(', ')}.
 Name is ${searchParams.get('name') || 'Anonymous'}.
-It has been determined that the patient has been diagnosed with the following: ${disease}.
+Determine what the patient may be suffering with. 
+diagnose the patient please!!
 Now, explain it to the patient in a way that they can understand.
+You are talking to the patient directly in a chat message.
+Moreover, you should also recommend what type of doctor they should see next.
 `;
 
     return (
@@ -22,11 +26,10 @@ Now, explain it to the patient in a way that they can understand.
             <div className="flex justify-center items-center w-full md:pt-20">
                 <div className="w-2/5">
                     <div className="mb-3">
-                        <h1 className="text-2xl font-bold">
-                            Problems
-                        </h1>
+                        <h1 className="text-2xl font-bold">Problems</h1>
                         <p className="text-sm">
-                            The following problems have been identified in your text:
+                            The following problems have been identified in your
+                            text:
                         </p>
                         <ul className="border-2 border-black rounded-md p-2 mt-3">
                             {problemList.map((problem, index) => (
@@ -40,5 +43,5 @@ Now, explain it to the patient in a way that they can understand.
                 </div>
             </div>
         </main>
-    )
+    );
 }
--- ml/bert.py
+++ ml/bert.py
@@ -1,13 +1,21 @@
 from fuzzywuzzy import process
 import nltk
 from nltk.corpus import stopwords
+import pandas as pd
 
-
-def get_symptoms():
+def get_general_symptoms():
     with open(".words", "r") as f:
         symptoms = f.readlines()
 
     return [symptom.strip() for symptom in symptoms]
+
+def get_symptoms():
+    df = pd.read_csv("Training.csv")
+    # get all of the column names
+    symptoms = list(df.columns[:-1])
+    symptoms = [symptom.replace("_", " ") for symptom in symptoms]
+    symptoms.extend(get_general_symptoms())
+    return symptoms
 
 
 def find_symptoms(sentence):
@@ -16,7 +24,7 @@ def find_symptoms(sentence):
 
     matched = process.extract(sentence, symptoms)
     for symptom, score in matched: # type: ignore
-        if score > 80:
+        if score > 90:
             matched_symptoms.add(symptom)
 
     return list(matched_symptoms)
